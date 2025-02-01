db6c0957b56555bad821cc4251e106dc3871f085
anteujcic41 <ante.ujcic@student.uniri.hr>
2025-02-01 18:37:32 +0100

    Add logi nredirect home page and base.html

--- barberapp/barberapp/settings.py
+++ barberapp/barberapp/settings.py
@@ -119,7 +119,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -130,7 +129,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/barberwebapp/home.html'
+LOGIN_REDIRECT_URL = 'home/'
 LOGOUT_REDIRECT_URL = '/barberwebapp/login.html'
 
 # Authentication and Permissions
--- barberapp/barberwebapp/urls.py
+++ barberapp/barberwebapp/urls.py
@@ -15,7 +15,7 @@ from . import urls_api
 app_name = 'barberwebapp'
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
